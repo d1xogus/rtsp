3a887f08684f370542e149ec54b015f5e17d5dc6
d1xogus <dlxo1012@naver.com>
2025-05-06 15:08:11 +0900

    rtsp명령어 수정

--- src/main/java/stream/StreamController.java
+++ src/main/java/stream/StreamController.java
@@ -51,7 +51,7 @@ public class StreamController {
                     String.format(
                             "cd ~/amazon-kinesis-video-streams-webrtc-sdk-c/build && " +
                                     "./samples/kvsWebrtcClientMasterGstSample " +
-                                    "--channel-name=%s " +
+                                    "--channel-name \"%s\" " +       // 값을 따옴표로 감쌈
                                     "--trickle-ice " +
                                     "--use-camera " +
                                     "--camera-source \"%s\" " +
